fb50204150376f68cf014772065b21f46432ddd9
Niklas <niklas.frank@gmail.com>
2016-06-05 08:53:37 +0200

    Initial commit

---
+++ PentiaCodeTest/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PentiaCodeTest
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PentiaCodeTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PentiaCodeTest.MvcApplication" Language="C#" %>
